38642b8a1c8e952a491a04c96e3152f22f667ed1
Manz Günter <gunterznam@gmail.com>
2018-09-11 21:47:32 +0200

    fixed jquery error on plugin load cuasing the threshold coloring to be non-functional.

--- src/sonarqube-gui-plugin/src/main/resources/static/loader.js
+++ src/sonarqube-gui-plugin/src/main/resources/static/loader.js
@@ -49,6 +49,7 @@ SM.loader.src = {
   },
   local: { // these get baseUrl prepended automatically
     scripts: [
+      'init.js',
       'lib/highlightjs/highlight.pack.js',
       'lib/diff2html/diff2html.min.js',
       'lib/diff2html/diff2html-ui.min.js',
@@ -157,28 +158,3 @@ SM.loader.loadSources = function(sources, base) {
 
 SM.loader.loadSources(SM.loader.src, SM.basePath);
 
-/*
- *  Load the Usersguides into variables for later use. Ideally theese would
- *  be displayed as the help pages on the SourceMeter Help page, but I imple-
- *  mented this function after I wrote the SourceMeter Help page, and couldnt
- *  figure out a fast and easy way to refactor the code, so refacoring remains
- *  a todo for later. The usersguides loaded here are used by the Metric.js
- *  class, to fetch tooltip data.
- */
-SM.languages.forEach(function(lang, i) {
-  $.get( '/static/' + lang.pluginId + '/help/usersguide.html')
-    .done(function(data, status, xhr) {
-      SM.languages[i].helpPage = data;
-      SM.languages.helpPageLoaded[lang.id] = true;
-    })
-    .fail(function(data, status, xhr) {
-      SM.languages[i].helpPage = [
-        '<div class="ui-state-error ui-state-highlight ui-corner-all" style="padding:10px"> ',
-        '  In order to see the users guide for the SourceMeter ' + lang.symbol + ' ',
-        '  scanner plugin, please put the ',
-        '  sourcemeter-analyzer-' + lang.id.toLowerCase() + '-plugin-1.0.0.jar',
-        '  into the extensions/plugins directory of your SonarQube instance. ',
-        '</div>'
-      ].join('/n');
-    });
-});
