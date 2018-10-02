72e467587adb94a6ab61a23a81c4df4faf1a204e
redLeR1st <bartab@inf.u-szeged.hu>
2018-10-02 18:52:33 +0200

    Improved testing tool. (#11)
    Implemented
    -SonarScanner download
      -Unzip both server and scanner
      -Copy plugins from a folder specified in arguments
      -Start the downloaded SQ server
      -Test the running of sq server
      -Analyze the given project
    
    Updated travis file for regression testing
    
    Added:
      -common.py file for the common functions and globals
      -Arguments for:
        -init
        -Server vesrsion
        -Scanner version
        -Plugin's folder
        -Project's folder (to be analyzed)
        -Number of attempts to check the running of SQ server
        -Waiting time between checking the running of SQ server
        -Folder for downloading and unzipping SQ server and scanner

--- tools/run-test.py
+++ tools/run-test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2014-2016, FrontEndART Software Ltd.
+# Copyright (c) 2014-2018, FrontEndART Software Ltd.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -29,15 +29,37 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import argparse
-import urllib2
+from urllib.request import urlopen
 import os
 import shutil
 import subprocess
+import zipfile
+from time import sleep
+import platform
+
+import common
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--init', action='store_true',
+    parser.add_argument('--init', action='store_true', default=False,
                         help='Initialize')
+    parser.add_argument('-sv', '--server-version', default='7.2',
+                        help='Version of the SQ server (default=%(default)s)')
+    parser.add_argument('-scv', '--scanner-version', default='3.2.0.1227',
+                        help='Version of the SQ scanner (default=%(default)s)')
+    parser.add_argument('-plgf', '--plugins-folder', default=None,
+                        help='Folder containing the plugins to be tested (default=%(default)s)')
+    parser.add_argument('-prjf', '--projects-folder', default='projectsfolder',
+                        help='Folder containing the project to be analyzed (default=%(default)s)')
+    parser.add_argument('-noa', '--number-of-attempts', default=20,
+                        help='Number of checking the status of SonarQube server (default=%(default)s times)')
+    parser.add_argument('-wait', '--wait', default=10,
+                        help='Wait between checking the status of SonarQube server (default=%(default)s seconds)')
+    parser.add_argument('-cf', '--client-folder', default='test',
+                        help='Folder for downloading and unzipping SQ server and scanner (default=%(default)s)')
+    parser.add_argument('--print-log', action='store_true',
+                        help='Prints the SQ log files to the screen')
+
     return parser.parse_args()
 
 def run_check(runnable):
@@ -50,22 +72,166 @@ def run_check(runnable):
 
     return ret
 
-def download_sq_server(version):
-    sq = urllib2.urlopen('https://sonarsource.bintray.com/Distribution/'
+def download_sq_server(version, dst):
+    print('Downloading: SonarQube server (version %s)...' % version)
+    sq = urlopen('https://sonarsource.bintray.com/Distribution/'
                          'sonarqube/sonarqube-%s.zip' % version)
     dl = sq.read()
-    with open('sonarqube-%s.zip' % version, 'w') as f:
+    path = os.path.join(dst, 'sonarqube-%s.zip' % version)
+    with open(path, 'wb') as f:
         f.write(dl)
+    print('SonarQube server download is completed! (version %s)' % version)
+
+def download_sq_scanner(version, system, dst):
+    system = system.lower()
+    print('Downloading: SonarQube scanner (version %s)...' % version)
+    sq = urlopen('https://sonarsource.bintray.com/Distribution/'
+                         'sonar-scanner-cli/sonar-scanner-cli-%s-%s.zip' % (version, system))
+    dl = sq.read()
+    path = os.path.join(dst, 'sonar-scanner-cli-%s-%s.zip' % (version, system))
+    with open(path, 'wb') as f:
+        f.write(dl)
+    print('SonarQube scanner download is completed! (version %s)' % version)
+
+def unzip(file, dst):
+    print('Unzipping files...')
+    with zipfile.ZipFile(file, "r") as zip_ref:
+        zip_ref.extractall(dst)
+    dst = os.path.join(os.getcwd(), dst)
+    print('Unzip finished! (%s)' % dst)
+
+def copy_all_files_from_folder(src, dst):
+    if src:
+        src_files = os.listdir(src)
+        for file_name in src_files:
+            full_file_name = os.path.join(src, file_name)
+            if (os.path.isfile(full_file_name)):
+                shutil.copy(full_file_name, dst)
+    else:
+        plugins = ['core', 'gui']
+        for plugin in plugins:
+            path = ['src', 'sonarqube-%s-plugin' % plugin, 'target', 'sourcemeter-%s-plugin-1.0.0.jar' % plugin]
+            path  = os.path.join(*path)
+            shutil.copy(path, dst)
+
+        languages = ['cpp', 'csharp', 'java', 'python', 'rpg']
+        for language in languages:
+            path = ['src', 'sonarqube-analyzers', 'sourcemeter-analyzer-%s' % language, 'target', 'sourcemeter-analyzer-%s-plugin-1.0.0.jar' % language]
+            path  = os.path.join(*path)
+            shutil.copy(path, dst)
+    print('Copy finished!')
+
+def start_sq_server(version, system, dst):
+    cmd = ''
+    cwd = os.getcwd()
+    if system == 'Windows':
+        sonar_location = cwd + '\\' + dst + '\\' + 'sonarqube-%s\\bin\\windows-x86-64\\StartSonar.bat' % version
+        common.run_cmd('start', [sonar_location])
+    elif system == 'Linux':
+        sonar_location = cwd + '/' + dst + '/sonarqube-%s' % version
+
+        temp = sonar_location
+        for root, dirs, files in os.walk(temp):
+            for file in files:
+                file_path = os.path.join(root, file)
+                os.chmod(file_path, 0o744)
+
+        cmd = os.path.join(sonar_location, 'bin/linux-x86-64/sonar.sh')
+        common.run_cmd(cmd, ['start', '&'])
+    print('Starting SQ server...')
+
+def validate_running_of_sq_server(version, number_of_attempts, wait):
+    number_of_attempts = int(number_of_attempts)
+    while not number_of_attempts == 0:
+        try:
+            contents = urlopen('http://localhost:9000/api/system/ping').read()
+            return True
+        except:
+            print('SonarQube is not started yet, rechecking...' + ' (%d attempt(s) left)' % number_of_attempts)
+            number_of_attempts -= 1
+            sleep(float(wait))
+    return False
+
+def analyze(scanner_version, project_folder, system, dst):
+    cmd = ''
+    cwd = os.getcwd()
+    scanner_location =  ''
+    if system == 'Windows':
+        scanner_location = cwd  + '\\' + dst + '\\sonar-scanner-%s-windows\\bin\\sonar-scanner.bat' % scanner_version
+        os.chdir(project_folder)
+        common.run_cmd('start', [scanner_location])
+    elif system == 'Linux':
+        cmd = cwd + '/' + dst + '/sonar-scanner-%s-linux/bin/sonar-scanner&' % scanner_version
+        os.chdir(project_folder)
+        common.run_cmd(cmd, [])
+    os.chdir('..')
+
+def print_log(version, path):
+    path = os.path.join(path, 'sonarqube-%s' % version, 'logs')
+    for file in [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]:
+        print('File name: ' + file)
+        with open(os.path.join(path, file), 'r') as f_in:
+            print(f_in.read())
 
 def main(options):
-    # 0) Try to build the plugins with 'build.py'
+    server_version = options.server_version
+    scanner_version = options.scanner_version
+    src_of_the_plugins = options.plugins_folder
+    src_of_the_project = options.projects_folder
+    noa = options.number_of_attempts
+    wait = options.wait
+    system = platform.system()
+    dst = options.client_folder
+    print_log_files = options.print_log
+    common.mkdir(dst)
 
-    download_sq_server('7.2.1')
-    # 1) download sonar-scanner
-    # 2) unzip both server and scanner
+    # 0, a) Try to build the plugins with 'build.py'
+
+    if system == 'Windows':
+        common.run_cmd('py', ['-3', 'build.py', '--all'])
+    elif system == 'Linux':
+        common.run_cmd('python3', ['tools/build.py', '--all'])
+
+    if options.init == True:
+        # 0, b) download sonar-server'
+
+        download_sq_server(server_version, dst)
+
+        # 1) download sonar-scanner
+
+        download_sq_scanner(scanner_version, system, dst)
+
+        # 2) unzip both server and scanner
+
+        src = os.path.join(dst, 'sonarqube-%s.zip' % server_version)
+        unzip(src, dst)
+        if 'Windows' == system:
+            src = os.path.join(dst, 'sonar-scanner-cli-%s-windows.zip' % scanner_version)
+        elif 'Linux' == system:
+            src = os.path.join(dst, 'sonar-scanner-cli-%s-linux.zip' % scanner_version)
+        unzip(src, dst)
+
     # 3) copy the plugins into the server dir
+
+    path = [dst, 'sonarqube-%s' % server_version, 'extensions', 'plugins']
+    path = os.path.join(*path)
+    copy_all_files_from_folder(src_of_the_plugins, path)
+
     # 4) start the server with the defult config
+
+    start_sq_server(server_version, system, dst)
+
     # 5) Validate the server is started succesfully
+    # 6) Analyze the given project
+
+    sleep(60)
+    if validate_running_of_sq_server(server_version, noa, wait):
+        print('SonarQube started properly!')
+    else:
+        print(('SonarQube did not start in time (-noa=%s (number of attempts))' % (noa)))
+        if print_log_files:
+            print_log(server_version, dst)
+        exit(1)
 
 if __name__ == "__main__":
     main(get_arguments())
